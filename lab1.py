37eb9fe43a11687f0e394beacd8b57244a7373df
zma9216 <71898694+zma9216@users.noreply.github.com>
2021-09-09 21:48:35 -0600

    Second commit

--- lab1.py
+++ lab1.py
@@ -1,5 +1,8 @@
 import requests
 
 print("Requests version is " + requests.__version__)
-response = requests.get("https://www.google.com/")
-print(response.status_code)
+response_1 = requests.get("https://www.google.com/")
+print(response_1.status_code)
+
+response_2 = requests.get("https://raw.githubusercontent.com/zma9216/Lab1/main/lab1.py")
+print("\n" + response_2.text)
